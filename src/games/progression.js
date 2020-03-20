545ac886ac0107b55ee65d7aa690ab8850b73f7c
AlekseyL <mare-spb@yandex.ru>
2020-03-20 17:39:35 +0300

    correction after verification version 2.3

--- src/games/progression.js
+++ src/games/progression.js
@@ -3,25 +3,29 @@ import flow from '../flow.js';
 
 const description = 'What number is missing in the progression?';
 
+function period() {
+  let numberOne = randomNumber(1, 20);
+  const periodNum = randomNumber(1, 100);
+  const arrPeriod = [];
+  for (let k = 0; k < 10; k += 1) {
+    arrPeriod.push(numberOne);
+    numberOne += periodNum;
+  }
+  return arrPeriod;
+}
+
 export default function brainProgression() {
   const arr = [];
   for (let i = 0; i < 3; i += 1) {
     const newArr = [];
-    const arrNum = [];
-    let numberOne = randomNumber(1, 20);
-    const period = randomNumber(1, 100);
+    const arrNum = period();
     const numberHidden = randomNumber(2, 8);
     let answerHiddenNumber;
     let str = '';
-    for (let k = 0; k < 10; k += 1) {
-      arrNum.push(numberOne);
-      numberOne += period;
-    }
     for (let g = 0; g < arrNum.length; g += 1) {
       if (g === numberHidden) {
         answerHiddenNumber = arrNum[g];
-        str += '..';
-        str += ' ';
+        str += '.. ';
       } else {
         str += arrNum[g];
         str += ' ';
